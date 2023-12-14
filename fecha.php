2b232636dd421b4e88d0ba3dd4c8ad9b8f55cd15
zuri-M <153556685+zuri-M@users.noreply.github.com>
2023-12-14 12:44:28 -0600

    Add files via upload

---
+++ fecha.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Problema</title>
+</head>
+<body>
+    <?php
+    echo"La fecha de hoy es:";
+    $fecha=date("d/m/y");
+    echo$fecha;
+    echo"<br>";
+    echo"La fecha actual es:";
+    $hora=date("H:i:s");
+    echo$hora;
+    echo"<br>";
+    ?>
+    <a href="fecha2.php">Siguiente problema </a>
+</body>
+</html>
